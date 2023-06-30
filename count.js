9aa98c0f7bb482b015129e6dd9015a1fb037faad
Souvik De <souvik.de@sato-global.com>
2023-07-01 00:36:11 +0900

    Final Commit for the Blackjack Game

--- count.js
+++
@@ -1,9 +0,0 @@
-let airbnbCastles = {
-    title:"live like a king",
-    price: 190,
-    isHost: true,
-    reviews:["img/castle1.png","img/castle2.png"]
-}
-
-console.log(airbnbCastles.isHost)
-console.log(airbnbCastles.price)
